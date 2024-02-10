d81bf0e36065cf29f3beed27aaeabec0cb15a3e0
Krish-Anto <127953560+Krish-Anto@users.noreply.github.com>
2024-02-10 07:34:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "KRISHNARAJ N",
+      "email": "krishnarajan1995@gamil.com",
+      "phone": 9629764172,
+      "degree": "B.E",
+      "location": {
+        "address": "101 A EVR STREET,",
+        "postalCode": 639116,
+        "city": "Karur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit,
+          "github":"https://github.com/Krish-Anto"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Wipro DOP",
+        "position": "Senior Officer",
+        "startDate": "2021-08-04",
+        "endDate": "Current",
+        "summary": "i am the Senior Officer and performing SNOW ticket handling task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Institute of Road and Transport Technology",
+        "department": "Automobile Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2012,
+        "batch end year": 2016,
+        "gpa": 6.23,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML,CSS,javascript,REACT,EXPRESS,MONGODB",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using JavaScript language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Music Production, Playing Basketball",
+      }
+    ]
+  }
+  console.log(myResume);
